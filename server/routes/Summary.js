3b8b8181e38a1c23550f4e959606716feb318bf2
wasif903 <wasifmehmood903@gmail.com>
2023-10-03 15:25:21 -0700

    lot of progreess

--- server/routes/Summary.js
+++ server/routes/Summary.js
@@ -6,51 +6,6 @@ import axios from "axios";
 
 const router = express.Router();
 
-// router.post("/summary", async (req, res) => {
-//   try {
-//     const transcript = await YoutubeTranscript.fetchTranscript(req.body.vidURL);
-//     const joined = transcript.map((item) => item.text).join(" ");
-
-//     const options = {
-//       method: "POST",
-//       url: "https://api.edenai.run/v2/text/summarize",
-//       headers: {
-//         authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiY2Q2YzhmOTAtNDcwMC00MzU5LWE1MTYtY2ExZDMyYjc5NGY5IiwidHlwZSI6ImFwaV90b2tlbiJ9.dE31lltMIxj6-uBubCaAXMHsYBKPQHzAOzpkZPpBMEg`,
-//       },
-//       data: {
-//         show_original_response: false,
-//         fallback_providers: "",
-//         providers: "openai",
-//         text: joined, // Remove the "i want summary in" part
-//         language: "en",
-//       },
-//     };
-
-//     const axiosRes = await axios.request(options);
-
-//     const summary = axiosRes.data.openai.result;
-
-//     // Get the desired word count from the request body
-//     const desiredWordCount = req.body.wordCounter;
-
-//     // Split the summary into words
-//     const words = summary.split(" ");
-//     // Check if the summary has more words than desired
-//     if (words.length > desiredWordCount) {
-//       // Trim the summary to the desired word count
-//       const trimmedSummary = words.slice(0, desiredWordCount).join(" ");
-//       const cleanedText = trimmedSummary.replace(/:/g, "");
-//       cleanedText2 = cleanedText.replace(/\n/g, " ");
-
-//       res.status(200).json(cleanedText2);
-//     } else {
-//       res.status(200).json(cleanedText2);
-//     }
-//   } catch (error) {
-//     console.log(error);
-//     res.status(500).json({ message: "Internal Server Error" });
-//   }
-// });
 let cleanedText2;
 router.post("/summary", async (req, res) => {
   try {
@@ -148,4 +103,5 @@ router.post("/summary", async (req, res) => {
     res.status(500).json({ message: "Internal Server Error" });
   }
 });
+
 export default router;
