32f7302aa1affbb40572da1240323deab081dc8c
Hasan <imtiazhasan873@gmail.com>
2023-10-04 03:55:06 +0500

    pull

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import style from "./Home.module.css";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const navigate = useNavigate();
   return (
     <div>
-      <h1>okokokokokokok</h1>
+      <h1 className={`${style.heading}`}>okokokokokokok</h1>
+      <button onClick={() => navigate("/inner-page")}>next</button>
     </div>
   );
 };
