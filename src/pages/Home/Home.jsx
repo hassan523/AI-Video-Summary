ed97852d84c443327b1586476ef9a3011536a9d0
Hassan <59671215+hassan523@users.noreply.github.com>
2023-10-04 20:55:08 +0500

    push

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router-dom";
 import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
 import Button from "react-bootstrap/Button";
+import { Container } from "react-bootstrap";
 
 const Home = () => {
   const navigate = useNavigate();
@@ -12,75 +13,77 @@ const Home = () => {
   const [onSubmit, setonSubmit] = useState(false);
 
   return (
-    <div className={style.box}>
-      <div>
-        <Form.Label htmlFor="basic-url">Paste Youtube URL here</Form.Label>
-        <InputGroup className="mb-3">
-          <InputGroup.Text id="basic-addon3">URL</InputGroup.Text>
-          <Form.Control
-            id="basic-url"
-            aria-describedby="basic-addon3"
-            placeholder="https://www.youtube.com/"
-            // onChange={(e) => seturl(e.target.value)}
-          />
-        </InputGroup>
-      </div>
-      <div className={style.rad}>
-        {["radio"].map((type) => (
-          <div key={`inline-${type}`} className="mb-3">
-            <Form.Check
-              inline
-              label="Bullet Points"
-              name="group1"
-              type={type}
-              id={`inline-${type}-1`}
-              onChange={() => setCheck(1)}
+    <Container className="d-flex align-items-center justify-content-center">
+      <div className={style.box}>
+        <div className="w-100">
+          <Form.Label htmlFor="basic-url">Paste Youtube URL here</Form.Label>
+          <InputGroup className="mb-3">
+            <InputGroup.Text id="basic-addon3">URL</InputGroup.Text>
+            <Form.Control
+              id="basic-url"
+              aria-describedby="basic-addon3"
+              placeholder="https://www.youtube.com/"
+              // onChange={(e) => seturl(e.target.value)}
             />
-            <Form.Check
-              inline
-              label="Paragraph form"
-              name="group1"
-              type={type}
-              id={`inline-${type}-2`}
-              onChange={() => setCheck(2)}
-            />
-          </div>
-        ))}
-        {check !== 0 ? (
-          <div className={style.aftercheck}>
-            {check === 1 ? (
-              <InputGroup size="sm" className="mb-3">
-                <InputGroup.Text id="inputGroup-sizing-sm">
-                  Key points
-                </InputGroup.Text>
-                <Form.Control
-                  aria-label="Small"
-                  aria-describedby="inputGroup-sizing-sm"
-                />
-              </InputGroup>
-            ) : (
-              <InputGroup size="sm" className="mb-3">
-                <InputGroup.Text id="inputGroup-sizing-sm">
-                  Words Count
-                </InputGroup.Text>
-                <Form.Control
-                  aria-label="Small"
-                  aria-describedby="inputGroup-sizing-sm"
-                />
-              </InputGroup>
-            )}
+          </InputGroup>
+        </div>
+        <div className={style.rad}>
+          {["radio"].map((type) => (
+            <div key={`inline-${type}`} className="mb-3">
+              <Form.Check
+                inline
+                label="Bullet Points"
+                name="group1"
+                type={type}
+                id={`inline-${type}-1`}
+                onChange={() => setCheck(1)}
+              />
+              <Form.Check
+                inline
+                label="Paragraph form"
+                name="group1"
+                type={type}
+                id={`inline-${type}-2`}
+                onChange={() => setCheck(2)}
+              />
+            </div>
+          ))}
+          {check !== 0 ? (
+            <div className={style.aftercheck}>
+              {check === 1 ? (
+                <InputGroup size="sm" className="mb-3">
+                  <InputGroup.Text id="inputGroup-sizing-sm">
+                    Key points
+                  </InputGroup.Text>
+                  <Form.Control
+                    aria-label="Small"
+                    aria-describedby="inputGroup-sizing-sm"
+                  />
+                </InputGroup>
+              ) : (
+                <InputGroup size="sm" className="mb-3">
+                  <InputGroup.Text id="inputGroup-sizing-sm">
+                    Words Count
+                  </InputGroup.Text>
+                  <Form.Control
+                    aria-label="Small"
+                    aria-describedby="inputGroup-sizing-sm"
+                  />
+                </InputGroup>
+              )}
+            </div>
+          ) : null}
+        </div>
+        <div className={style.btn}>
+          <Button onClick={() => setonSubmit(true)}>Submit</Button>
+        </div>
+        {onSubmit ? (
+          <div className={style.summary}>
+            <h3>Summary</h3>
           </div>
         ) : null}
       </div>
-      <div className={style.btn}>
-        <Button onClick={() => setonSubmit(true)}>Submit</Button>
-      </div>
-      {onSubmit ? (
-        <div className={style.summary}>
-          <h3>Summary</h3>
-        </div>
-      ) : null}
-    </div>
+    </Container>
   );
 };
 
