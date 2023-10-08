6488319b67cadd651b566339b10d1e990a2f6a9e
Hasan <imtiazhasan873@gmail.com>
2023-10-08 21:27:28 +0500

    pull

--- src/routes/user.jsx
+++ src/routes/user.jsx
@@ -1,22 +1,13 @@
 import { Navigate } from "react-router-dom";
 import Home from "../pages/Home/Home";
-import InnerPage from "../pages/innerPage/InnerPage";
-import Login from "../component/Login";
-import Signup from "../component/Signup";
+import Signup from "../pages/Sign-up/Signup";
 
 const user = [
   {
     path: "/",
     element: <Home />,
   },
-  {
-    path: "/inner-page",
-    element: <InnerPage />,
-  },
-  {
-    path: "/login",
-    element: <Login />,
-  },
+
   {
     path: "/signup",
     element: <Signup />,
